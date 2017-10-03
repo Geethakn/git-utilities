bcc6d9c8dbf5645bf48fa74af434c461b5c7c7c7
Geethakn <32490778+Geethakn@users.noreply.github.com>
2017-10-03 17:41:03 -0500

    Update git-sisters-update.ps1

--- git-sisters-update.ps1
+++ git-sisters-update.ps1
@@ -1,4 +1,4 @@
-# updated by Me AGAIN
+# updated by Me AGAIN === FEATURES
 # git-sisters-update.ps1 from within http://github.com/wilsonmar/git-utilities.
 # by Wilson Mar (wilsonmar@gmail.com, @wilsonmar)
 
